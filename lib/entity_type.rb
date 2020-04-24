487a5c1e06f9101257e3a2b1ff6a4546316f7001
Andy Wang <andyw@seek.com.au>
2020-04-24 23:22:17 +1000

    Create USER_TYPE and TICKET_TYPE

--- lib/entity_type.rb
+++ lib/entity_type.rb
@@ -5,6 +5,15 @@ class EntityType
   ORGANIZATION_FIELDS = ['_id', 'url', 'external_id', 'name', 'domain_names',
                          'created_at', 'details', 'shared_tickets', 'tags']
 
+  USER_FIELDS         = ['_id', 'url', 'external_id', 'name', 'alias', 'created_at',
+                         'active', 'verified', 'shared', 'locale', 'timezone',
+                         'last_login_at', 'email', 'phone', 'signature', 'organization_id',
+                         'tags', 'suspended', 'role']
+
+  TICKET_FIELDS       = ['_id', 'url', 'external_id', 'created_at', 'type', 'subject',
+                         'description', 'priority', 'status', 'submitter_id', 'assignee_id',
+                         'organization_id', 'tags', 'has_incidents', 'due_at', 'via']
+
   attr_reader :name, :field_names
 
   def initialize(name, field_names)
@@ -16,5 +25,7 @@ class EntityType
     field_names.include?(String(field_name))
   end
 
-  ORGANIZATION_TYPE = EntityType.new('Organization', ORGANIZATION_FIELDS)
+  ORGANIZATION_TYPE   = EntityType.new('Organization', ORGANIZATION_FIELDS)
+  USER_TYPE           = EntityType.new('User', USER_FIELDS)
+  TICKET_TYPE         = EntityType.new('Ticket', TICKET_FIELDS)
 end
