7e4b3d3109a8c3900ca62c9c10544e3c5ab2ea59
Andy Wang <andyw@seek.com.au>
2020-04-26 15:38:41 +1000

    Add SearchManager

--- lib/search_manager.rb
+++ lib/search_manager.rb
@@ -1,5 +1,8 @@
 require_relative './entity_repository'
 require_relative './index_repository'
+require_relative './fields_enrichers/organization_enricher'
+require_relative './fields_enrichers/ticket_enricher'
+require_relative './fields_enrichers/user_enricher'
 
 ##
 # SearchManager is responsible of indexing and search entites
@@ -8,11 +11,24 @@ class SearchManager
   def initialize
     @entity_repository = EntityRepository.new
     @index_repository = IndexRepository.new
+    @fields_enrichers = {
+      'Organization'    => OrganizationEnricher.new(self),
+      'User'            => UserEnricher.new(self),
+      'Ticket'          => TicketEnricher.new(self)
+    }
   end
 
   def add_entity(entity)
     entity_repository.add_entity(entity)
     index_repository.index(entity)
+  end
+
+  def search_entities(entity_type_name, field_name, search_term)
+    entity_ids = search_entity_ids(entity_type_name, field_name, search_term)
+    entity_ids.map do |entity_id|
+      entity = fetch_entity(entity_type_name, entity_id)
+      fields_enricher_for(entity_type_name).enriched_fields(entity)
+    end
   end
 
   def search_entity_ids(entity_type_name, field_name, search_term)
@@ -25,5 +41,11 @@ class SearchManager
 
   private
 
-  attr_reader :entity_repository, :index_repository
+  attr_reader :entity_repository, :index_repository, :fields_enrichers
+
+  def fields_enricher_for(entity_type_name)
+    fields_enrichers.fetch(entity_type_name) do |_|
+      raise SearchError, "The fields enricher is not found for #{entity_type_name}"
+    end
+  end
 end
--- spec/search_manager_spec.rb
+++ spec/search_manager_spec.rb
@@ -1,9 +1,45 @@
-# describe SearchManager do
-#   subject { SearchManager.build }
+describe SearchManager do
+  let(:organizations) { load_entities_from_fixture(EntityType::ORGANIZATION_TYPE, 'organizations.json') }
+  let(:users)         { load_entities_from_fixture(EntityType::USER_TYPE, 'users.json') }
+  let(:tickets)       { load_entities_from_fixture(EntityType::TICKET_TYPE, 'tickets.json') }
 
-#   describe '#search' do
-#     it 'can search users' do
-#       expect(subject.search('User', '_id', '101')).to eq ['101']
-#     end
-#   end
-# end
+  subject { SearchManager.new }
+
+  before do
+    [organizations, users, tickets].each do |entities|
+      entities.each { |entity| subject.add_entity(entity) }
+    end
+  end
+
+  describe '#search_entities' do
+    it 'can search users' do
+      expected = [
+        {
+          '_id'                 => 1,
+          'active'              => true,
+          'alias'               => 'Miss Coffey',
+          'assigned_ticket_0'   => 'A Catastrophe in Macau',
+          'created_at'          => '2016-04-15T05:19:46 -10:00',
+          'email'               => 'coffeyrasmussen@flotonic.com',
+          'external_id'         => '74341f74-9c79-49d5-9611-87ef9b6eb75f',
+          'last_login_at'       => '2013-08-04T01:03:27 -10:00',
+          'locale'              => 'en-AU',
+          'name'                => 'Francisca Rasmussen',
+          'organization'        => 'Enthaze',
+          'organization_id'     => 101,
+          'phone'               => '8335-422-718',
+          'role'                => 'admin',
+          'shared'              => false,
+          'signature'           => "Don't Worry Be Happy!",
+          'submitted_ticket_0'  => 'A Catastrophe in Pakistan',
+          'suspended'           => true,
+          'tags'                => ['Springville', 'Sutton', 'Hartsville/Hartley', 'Diaperville'],
+          'timezone'            =>  'Sri Lanka',
+          'url'                 => 'http://initech.zendesk.com/api/v2/users/1.json',
+          'verified'            => true
+        }
+      ]
+      expect(subject.search_entities('User', '_id', '1')).to eq expected
+    end
+  end
+end
